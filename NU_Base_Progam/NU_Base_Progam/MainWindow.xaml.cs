d0779eb526d8610ea0519eb72fdbba24efb1d092
Alexander Huertas <ahuertas000@gmail.com>
2017-04-03 18:42:32 -0600

    Committing default timer program just as a base ground for capstone.

---
+++ NU_Base_Progam/NU_Base_Progam/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace NU_Base_Progam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DispatcherTimer dtClockTime = new DispatcherTimer();
+
+            dtClockTime.Interval = new TimeSpan(0, 0, 1); //in Hour, Minutes, Second.
+            dtClockTime.Tick += dtClockTime_Tick;
+
+            dtClockTime.Start();
+        }
+
+        private void dtClockTime_Tick(object sender, EventArgs e)
+        {
+            lblClockTime.Content = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
