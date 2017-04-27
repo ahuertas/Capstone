2c15a5c690c069ee7bb85942852f9aff52b82772
Alexander Huertas <ahuertas000@gmail.com>
2017-04-26 21:23:09 -0600

    Made progress: 4 forms filled, notify function added to capstone_base. No errors as of yet

--- Capstone_Base/Capstone_Base/MainWindow.xaml.cs
+++ Capstone_Base/Capstone_Base/MainWindow.xaml.cs
@@ -14,9 +14,11 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Capstone_Base.MainDetails;
 using CanvasApiLib.Objects;
-using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Threading;
 using System.Threading;
+using System.Drawing;
+using System.ComponentModel;
 
 namespace Capstone_Base
 {
@@ -27,28 +29,24 @@ namespace Capstone_Base
     {
         private static Object mylock = new Object();
         private string token;
-        private string classes = " ";
-        private string tasks = " ";
+        private NotifyIcon MyNotifyIcon;
         private StudentInfoHandler studentInfo;
-        private List<SchoolCourse> currentClasses = new List<SchoolCourse>();
-        private List<Assignment> currentTasks = new List<Assignment>();
-
-        public MainWindow()
-        {
-
-        }
+        private List<SchoolCourse> currentClasses;
+        private List<Assignment> currentTasks;
+        private bool _isExit;
 
         public  MainWindow(string token)
         {
             this.token = token;
             InitializeComponent();
+            this.Closing += MainWindow_Closing;
+        
+            currentClasses  = new List<SchoolCourse>();
+            currentTasks = new List<Assignment>();
+
             Run().Wait();
-
-            //classBox.Items.Add(classes);
-            //Console.WriteLine(classes);
-
-            //taskBox.Items.Add(tasks);
-            //Console.WriteLine(tasks);
+            NotifationSetUp();
+            CreateContextMenu();
         }
 
         public async Task Run()
@@ -90,11 +88,62 @@ namespace Capstone_Base
                 }
                 catch (Exception err)
                 {
-                    MessageBox.Show(err.Message);
+                    System.Windows.MessageBox.Show(err.Message);
                 }
 
             }
             ));
+        }
+       
+        private void NotifationSetUp()
+        {
+            MyNotifyIcon = new System.Windows.Forms.NotifyIcon();
+            MyNotifyIcon.DoubleClick += (s, args) => DisplayMainWindow();
+            MyNotifyIcon.Icon = new Icon("Buddybleu.ico");
+            MyNotifyIcon.Visible = true;
+        }
+
+        private void CreateContextMenu()
+        {
+            MyNotifyIcon.ContextMenuStrip =
+                new System.Windows.Forms.ContextMenuStrip();
+            MyNotifyIcon.ContextMenuStrip.Items.Add("MainWindow").Click += (s, e) => DisplayMainWindow();
+
+            MyNotifyIcon.ContextMenuStrip.Items.Add("Exit").Click += (s, e) => ExitApp();
+        }
+
+
+        private void DisplayMainWindow()
+        {
+            if(IsVisible)
+            {
+                if(this.WindowState == WindowState.Minimized)
+                {
+                    this.WindowState = WindowState.Normal;
+                }
+                this.Activate();
+            }
+            else
+            {
+                Show();
+            }
+        }
+
+        private void ExitApp()
+        {
+            _isExit = true;
+            this.Close();
+            MyNotifyIcon.Dispose();
+            MyNotifyIcon = null;
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if(!_isExit)
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
         }
 
     }
